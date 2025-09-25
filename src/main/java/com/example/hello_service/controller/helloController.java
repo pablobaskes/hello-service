86c7d3657228fd899e5a6a28c46c4ca8e2fc0013
Pablo <pablovazquezbarrena97@gmail.com>
2025-09-25 18:02:05 +0200

    controller done

--- src/main/java/com/example/hello_service/controller/helloController.java
+++ src/main/java/com/example/hello_service/controller/helloController.java
@@ -1,4 +1,15 @@
 package com.example.hello_service.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class helloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello(){
+        ResponseEntity<String> response = ResponseEntity.ok("Hello Wolrd!");
+        return response;
+    }
 }
